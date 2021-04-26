172e73fda743bece78d265094cf943a177340d48
Aliakwe <74242021+Aliakwe@users.noreply.github.com>
2021-04-26 22:23:56 +0100

    update: implement patch and delete

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,5 @@
 const express = require('express');
+// const subscriber = require('../models/subscriber');
 const router = express.Router();
 const Subscriber = require('../models/subscriber');
 
@@ -14,8 +15,8 @@ router.get('/', async (req, res) => {
 });
 // Getting one
 
-router.get('/:id', async (req, res) => {
-  res.send(req.params.id);
+router.get('/:id', getSubscriber, (req, res) => {
+  res.send(res.subscriber);
 });
 
 // Creating one
@@ -35,9 +36,41 @@ router.post('/', async (req, res) => {
 });
 // Updating one
 
-router.patch('/id', (req, res) => {});
+router.patch('/:id', getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 // Deleting one
 
-router.delete('/id', (req, res) => {});
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({ message: 'Deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: 'Cannot find subscriber' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
